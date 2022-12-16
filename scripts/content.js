2b2353144a17fadaa38bf6505e5e0453a70d22ae
Mario IC <mario21ic@gmail.com>
2022-12-15 21:48:39 -0500

    ready

---
+++ scripts/content.js
@@ -0,0 +1,7 @@
+console.log("BVD - reading dom");
+setTimeout(() => {
+    console.log("Getting video..");
+    player_wrap = document.querySelector(".vjs-tech");
+    console.log(player_wrap.src);
+}, 7000);
+
